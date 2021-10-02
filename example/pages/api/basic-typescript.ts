67d3f88bae3400adc2f33c0d0f0a77e3b2c4faf3
Igor Klepacki <neg4n@icloud.com>
2021-10-02 16:54:03 +0200

    Update example

--- example/pages/api/basic-typescript.ts
+++ example/pages/api/basic-typescript.ts
@@ -1,20 +1,12 @@
 import { withOGImage } from 'next-api-og-image'
 
-export default withOGImage<'testQueryParam'>(
-  (request, response) => {
-    response.setHeader('Content-Type', 'image/png')
-    response.setHeader('Cache-Control', 'max-age 3600, must-revalidate')
-    response.write(request.ogimage)
-    response.end()
-  },
-  {
-    html: ({ testQueryParam }) => {
-      return `
+export default withOGImage<'testQueryParam'>({
+  html: async ({ testQueryParam }) => {
+    return `
       <html>
         <body>
           <h1>${testQueryParam}</h1>
         </body>
       </html>`
-    },
   },
-)
+})
