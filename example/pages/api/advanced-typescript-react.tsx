b744cc7228b8f739d9aa59337c3eae100f494053
Igor Klepacki <neg4n@icloud.com>
2021-10-14 05:47:15 +0200

    Add new examples using react templates and fix existing (#6)

---
+++ example/pages/api/advanced-typescript-react.tsx
@@ -0,0 +1,56 @@
+import { withOGImage } from 'next-api-og-image'
+import { interRegular } from '../../fonts/inter'
+
+const style = `
+  @font-face {
+    font-family: 'Inter';
+    font-style:  normal;
+    font-weight: normal;
+    src: url(data:font/woff2;charset=utf-8;base64,${interRegular}) format('woff2');
+  }
+
+  body {
+    font-family: 'Inter', sans-serif;
+  }
+
+  .container {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+export default withOGImage<'testQueryParam'>({
+  template: {
+    react: async ({ testQueryParam }) => {
+      const value = await someLongRunningValueGetter()
+      return (
+        <html>
+          <head>
+            <style dangerouslySetInnerHTML={{ __html: style }} />
+          </head>
+          <body>
+            <div className="container">
+              <h1>{testQueryParam}</h1>
+              <h2>{value}</h2>
+            </div>
+          </body>
+        </html>
+      )
+    },
+  },
+  dev: {
+    inspectHtml: false,
+  },
+})
+
+function someLongRunningValueGetter() {
+  return new Promise((resolve: (value: string) => void) => {
+    setTimeout(() => {
+      resolve("Value in setTimeout's (500ms) callback")
+    }, 500)
+  })
+}
--- example/pages/api/basic-typescript.ts
+++ example/pages/api/basic-typescript.ts
@@ -1,13 +1,15 @@
 import { withOGImage } from 'next-api-og-image'
 
 export default withOGImage<'testQueryParam'>({
-  html: async ({ testQueryParam }) => {
-    return `
-      <html>
-        <body>
-          <h1>${testQueryParam}</h1>
-        </body>
-      </html>
-    `
+  template: {
+    html: async ({ testQueryParam }) => {
+      return `
+        <html>
+          <body>
+            <h1>${testQueryParam}</h1>
+          </body>
+        </html>
+      `
+    },
   },
 })
