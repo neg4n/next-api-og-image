6698e8f4205d17962e0299fbc19784a6eb501cef
Bart Stefanski <contact@bstefanski.com>
2021-10-27 01:15:53 +0200

    add basic-styled-components example

---
+++ example/pages/api/basic-styled-components.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { withOGImage } from 'next-api-og-image'
+import { renderToString } from 'react-dom/server'
+import styled, { ServerStyleSheet } from 'styled-components'
+
+function Markup({ myQueryParam }) {
+  return <Wrapper>🔥 {myQueryParam}</Wrapper>
+}
+
+export default withOGImage({
+  template: {
+    html: ({ myQueryParam }) => {
+      const sheet = new ServerStyleSheet()
+      try {
+        const html = renderToString(sheet.collectStyles(<Markup myQueryParam={myQueryParam} />))
+        const styleTags = sheet.getStyleTags()
+
+        return `<html><style>body {margin:0}</style>${styleTags}<body>${html}</body></html>`
+      } catch (error) {
+        console.error(error)
+        return 'error'
+      } finally {
+        sheet.seal()
+      }
+    },
+  },
+})
+
+const Wrapper = styled.div`
+  box-sizing: border-box;
+  background: #fafafa;
+  font-size: 5rem;
+  border: 10rem solid red;
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+`
